5e98edeb9d125469fd0b0e9fad7c0637e0066721
PhamPhuTuan <phamphutuan0710pt@gmail.com>
2022-11-25 22:28:59 +0700

    update

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRRpx_31ipA9hENZDoJ8Bx6hW6qI83K0g",
-  authDomain: "nextfix-fa052.firebaseapp.com",
-  projectId: "nextfix-fa052",
-  storageBucket: "nextfix-fa052.appspot.com",
-  messagingSenderId: "283156001064",
-  appId: "1:283156001064:web:467b40b256bdb31fba201d",
-  measurementId: "G-LN0QN4DTBG"
+  apiKey: "AIzaSyDsTtgCf60-8KAanolmR101vJ5QvwAMw18",
+  authDomain: "apt-retina-340606.firebaseapp.com",
+  projectId: "apt-retina-340606",
+  storageBucket: "apt-retina-340606.appspot.com",
+  messagingSenderId: "617755638355",
+  appId: "1:617755638355:web:8ac4a8c9ea0148589be4a6",
+  measurementId: "G-E5HED7KY5T"
 };
 
 firebase.initializeApp(firebaseConfig);
